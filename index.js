0d31e5634b3eec6e70a58721a39a07aa8e299fba
Winston Pemberton <winstonpemberton@gmail.com>
2020-11-25 23:19:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+const event = "birthday";
+
+function writeCards(names, event) {
+  for (let i= 0; i < names.length; i++) {
+  birthday[i] = 'Thank you, ${gifts[i]} , for the wonderful ${gifts[i]} gift!';
+  console.log('Thank you, ${gifts[i]} , for the wonderful ${gifts[i]} gift!');
+  "Thank you, Ada, for the wonderful birthday gift!",
+  }
+  return birthday;
+}
