ceb5f383cd5955f579bf47aa0444ff8b9d6d7ea9
Winston Pemberton <winstonpemberton@gmail.com>
2020-11-25 23:23:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,4 @@ function writeCards(names, event) {
   for (let i= 0; i < names.length; i++) {
   console.log(`Thank you, ${names[i]} , for the wonderful ${event} gift!`);
   }
-  return birthday;
 }
